89dc2dc3c49ac69b64760aaa26ad0882c00d156b
dandy <danielsbarbosa27@gmail.com>
2021-01-12 04:20:17 -0300

    Grades de Images ok

--- loja_virtual_app/lib/tabs/home_tab.dart
+++ loja_virtual_app/lib/tabs/home_tab.dart
@@ -1,15 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget _buildBodyBack() => Container(
+    Widget _buildBodyBack() =>
+        Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 211, 118, 130),
-            Color.fromARGB(255, 253, 181, 168)
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                Color.fromARGB(255, 211, 118, 130),
+                Color.fromARGB(255, 253, 181, 168)
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
         );
 
     return Stack(
@@ -43,16 +46,21 @@ class HomeTab extends StatelessWidget {
                       ),
                     ),
                   );
-                else {
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                    child: Container(
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: Container(),
-                    ),
+                else
+                  return SliverStaggeredGrid.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    staggeredTiles: snapshot.data.documents.map((doc) {
+                      return StaggeredTile.count(doc.data['x'], doc.data['y']);
+                    }).toList(),
+                    children: snapshot.data.documents.map((doc) {
+                      return FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage, image: doc.data['image'],
+                          fit: BoxFit.cover,
+                      );
+                    }).toList(),
                   );
-                }
               },
             )
           ],
