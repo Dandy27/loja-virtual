ec253c91c0188e56397a11de3d5e7c156dd335ce
dandy <danielsbarbosa27@gmail.com>
2021-01-12 01:39:29 -0300

    Barra Flutuante

--- loja_virtual_app/lib/tabs/home_tab.dart
+++ loja_virtual_app/lib/tabs/home_tab.dart
@@ -19,7 +19,21 @@ class HomeTab extends StatelessWidget {
 
     return Stack(
       children: [
-        _buildBodyBack()
+        _buildBodyBack(),
+        CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text("Novidades"),
+                centerTitle: true,
+              ),
+            )
+          ],
+        )
       ],
     );
   }
