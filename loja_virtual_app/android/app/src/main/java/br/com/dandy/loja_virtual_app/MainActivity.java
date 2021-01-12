1f233f66ced6faf2924af45b22ec41c2ba3eca5c
dandy <danielsbarbosa27@gmail.com>
2021-01-12 01:01:54 -0300

    PageView ok

---
+++ loja_virtual_app/android/app/src/main/java/br/com/dandy/loja_virtual_app/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.dandy.loja_virtual_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ loja_virtual_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Clothing",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
