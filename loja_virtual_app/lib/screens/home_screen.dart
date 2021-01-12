8f5adbd380097ba1f6658c346099aa97105e8d15
dandy <danielsbarbosa27@gmail.com>
2021-01-12 01:16:40 -0300

    PageController ok

--- loja_virtual_app/lib/screens/home_screen.dart
+++ loja_virtual_app/lib/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
       children: [
         Container(
           color: Colors.yellow,
