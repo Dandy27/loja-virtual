daedca165d8c3bd082c8974f21e96b9b8df69650
dandy <danielsbarbosa27@gmail.com>
2021-01-12 03:25:37 -0300

    Obtendo as Imagens da Tela Inicial

--- loja_virtual_app/lib/tabs/home_tab.dart
+++ loja_virtual_app/lib/tabs/home_tab.dart
@@ -1,21 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     Widget _buildBodyBack() => Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
             Color.fromARGB(255, 211, 118, 130),
             Color.fromARGB(255, 253, 181, 168)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        )
-      ) ,
-    );
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+        );
 
     return Stack(
       children: [
@@ -31,6 +26,34 @@ class HomeTab extends StatelessWidget {
                 title: Text("Novidades"),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection("home")
+                  .orderBy("pos")
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                else {
+                  print(snapshot.data.documents.length);
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: Container(),
+                    ),
+                  );
+                }
+              },
             )
           ],
         )
