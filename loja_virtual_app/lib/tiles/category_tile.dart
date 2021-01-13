92913ef56e96f41ac73a7086a69368a6dc176b80
dandy <danielsbarbosa27@gmail.com>
2021-01-12 22:58:48 -0300

    Criando tela de produto ok

--- loja_virtual_app/lib/tiles/category_tile.dart
+++ loja_virtual_app/lib/tiles/category_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_app/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
 
@@ -17,7 +18,9 @@ class CategoryTile extends StatelessWidget {
       title: Text(snapshot.data['title']),
       trailing: Icon(Icons.keyboard_arrow_right),
       onTap: (){
-
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>CategoryScreen(snapshot))
+        );
       },
     );
   }
